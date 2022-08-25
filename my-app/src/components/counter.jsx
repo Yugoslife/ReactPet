d89ea7d93470dc36cd0f010719b8fbfee17a99b9
Lisica <vasetskiy.y@gmail.com>
2022-08-25 17:59:51 +0200

    added refactor getBadgeClasses

--- my-app/src/components/counter.jsx
+++ my-app/src/components/counter.jsx
@@ -7,12 +7,20 @@ class Counter extends Component {
 
 
     render() { 
-        return <React.Fragment>
 
-            <span style={{fontSize:30}} className='badge badge-primary m-2'>{this.formatCount()}</span>
-            <button style={{fontSize:30}} className='btn btn-primary btn-sm'>Increment</button>
-        </React.Fragment>;
+        return ( <div>
+            <span style={{fontSize:30}} className= { this.getBadgeClasses() }>{this.formatCount()}</span>
+            <button className= "btn btn-secondary btn-sm">Increment</button>
+        </div>
+        );
     }
+
+    getBadgeClasses() {
+        let classes = 'badge m-2 badge-';
+        classes += (this.state.count === 0) ? 'primary' : 'warning';
+        return classes;
+    }
+
     formatCount () {
         const { count } = this.state;
         return count === 0 ? 'Zero' : count;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
-import Counter from './components/counter';
+import Counter from './components/counter.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
