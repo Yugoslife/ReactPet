58784545ebb56bccb6ea9f81812c2963be8b127f
Lisica <vasetskiy.y@gmail.com>
2022-08-31 13:55:25 +0200

    added map and key for li

--- my-app/src/components/counter.jsx
+++ my-app/src/components/counter.jsx
@@ -1,23 +1,29 @@
 import React, { Component } from 'react';
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 class Counter extends Component {
     state = {
         count: 0,
+        tags: ['tag1', 'tag2', 'tag3']
      };
 
 
     render() { 
 
-        return ( <div>
-            <span style={{fontSize:30}} className= { this.getBadgeClasses() }>{this.formatCount()}</span>
+        return ( 
+        <div>
+            <span style={{fontSize:12}} className= { this.getBadgeClasses() }>{this.formatCount()}</span>
             <button className= "btn btn-secondary btn-sm">Increment</button>
+            <ul>
+            { this.state.tags.map(tag => <li key={tag}>{ tag }</li>)}
+            </ul>
         </div>
         );
     }
 
     getBadgeClasses() {
-        let classes = 'badge m-2 badge-';
-        classes += (this.state.count === 0) ? 'primary' : 'warning';
+        let classes = 'btn m-1 btn-';
+        classes += (this.state.count === 0) ? 'warning' : 'primary';
         return classes;
     }
 
