ff1f3a489aba81969e3085a549497cd622316c08
Lisica <vasetskiy.y@gmail.com>
2022-09-05 08:57:09 +0200

    added handleIncrement with count, bg-

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,10 +7,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <div>
           Click on Lisichka <code>src/App.js</code> and save to reload.
-          <div>Mous ist meine Frau</div>
-        </p>
+          <p>Mous ist meine Frau</p>
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
--- my-app/src/components/counter.jsx
+++ my-app/src/components/counter.jsx
@@ -2,26 +2,44 @@ import React, { Component } from 'react';
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 class Counter extends Component {
-    state = {
-        count: 0,
-        tags: ['tag1', 'tag2', 'tag3']
-     };
+    constructor(props) {
+        super(props);
+        this.state = {
+            count: 0
+        };
+      }
+   
 
-renderTags() {
-    if (this.state.tags.length === 0) return <p>There are no tags!</p>
-
-    return <ul> { this.state.tags.map(tag => <li key={tag}>{ tag }</li>)} </ul>;
+handleIncrement(){
+    console.log("Increment Clicked", this.state.count);
+    this.setState({
+        count: this.state.count +1
+    });
 }
 
-    render() { 
-
-        return ( 
+render() { 
+    return ( 
         <div>
-            {this.renderTags()}
+            <span className={this.getBadgeClasses()}> {this.state.count}</span>
+            <button 
+            onClick={()=>this.handleIncrement()}
+            className='btn btn-secondary btn-sm'
+            >
+            Increment
+            </button>
         </div>
         );
-    }
+}
 
+formatCount() {
+    console.log('Counted');
+}
+
+getBadgeClasses() {
+    let classes = 'badge m-2 bg-';
+    classes += this.state.count === 0 ? 'danger' : 'primary';
+    return classes;
+}
 }
  
 export default Counter; 
