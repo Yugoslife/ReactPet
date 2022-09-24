567efd232b1ef6d128d897e77c770f53bef22efa
Natalya Iskanderova <natalya_iskanderova@epam.com>
2022-09-24 22:27:40 +0200

    2h10min destructuring arguments

--- my-app/src/components/counters.jsx
+++ my-app/src/components/counters.jsx
@@ -1,20 +1,20 @@
 import React, { Component } from 'react';
 import Counter from './counter';
-
 class Counters extends Component {
     render() { 
+        const { onReset, counters, onDelete, onIncremet } = this.props;
         return (
         <div>
             <button 
-            onClick={this.props.onReset}
+            onClick={onReset}
             className="btn btn-primary btn-sm m-2">
                 Reset
             </button>
-        {this.props.counters.map(counter =>  ( 
+        {counters.map(counter =>  ( 
         <Counter 
         key={counter.id} 
-        onDelete={this.props.onDelete}
-        onIncremet={this.props.onIncremet}
+        onDelete={onDelete}
+        onIncremet={onIncremet}
         counter={counter}
         />
         ))}
--- my-app/src/components/navbar.jsx
+++ my-app/src/components/navbar.jsx
@@ -3,14 +3,14 @@ import React, { Component } from "react";
 // State Functional Component - 
 // - функциональный компонент без сохранения состояния
 
-const NavBar = (props) => {
+const NavBar = ({totalCounters}) => {
         return (
         <nav className="navbar navbar-light bg-light">
         <div className="container-fluid">
         <a className="navbar-brand" href="/something">
         Navbar {' '}
         <span className="badge bg-pill bg-secondary">
-            {props.totalCounters}
+            {totalCounters}
         </span>
         </a>
         </div>
