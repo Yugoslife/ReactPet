1e4c5415a3530bf8d0df54d26b061a92bd606c8d
Natalya Iskanderova <natalya_iskanderova@epam.com>
2022-09-24 21:53:40 +0200

    2h06min ade counter in navbar and Lifting state up

--- my-app/src/components/navbar.jsx
+++ my-app/src/components/navbar.jsx
@@ -4,11 +4,16 @@ class NavBar extends Component {
     render () {
         return (
         <nav className="navbar navbar-light bg-light">
-        <a className="navbar-brand" href="#">
+        <div className="container-fluid">
+        <a className="navbar-brand" href="/something">
         Navbar
+        <span className="badge bg-pill bg-secondary">
+            {this.props.totalCounters}
+        </span>
         </a>
+        </div>
         </nav>
-      )
+    )
     };
 }
 export default NavBar;
